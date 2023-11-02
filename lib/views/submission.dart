52a721a9b5011b25f7a57046a36bc17f10add1ac
upeksha434 <upeksha434@gmail.com>
2023-11-02 23:56:13 +0530

    forms batch upload

--- lib/views/submission.dart
+++ lib/views/submission.dart
@@ -25,85 +25,133 @@ class SubmissionPage extends StatelessWidget {
       appBar: AppBar(title: const Text('Unsynced Submissions')),
       body: submissions!.isEmpty
           ? Center(child: Text('No unsynced submissions.'))
-          : ListView.builder(
+          : Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: submissions.length,
+              itemBuilder: (context, index) {
+                final submission = submissions[index];
+                return Material(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.4),
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                        child: ListTile(
+                          enabled: true,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          title: Text('${FormViewController.formData?['title'] ?? 'No Title'}'),
+                          subtitle: Text('Submission ID: ${submission.id}'),
+                          trailing: IconButton(
+                            icon: Icon(Icons.upload, color: Colors.white, size: 30),
+                            onPressed: () async {
 
-        itemCount: submissions.length,
-        itemBuilder: (context, index) {
-          final submission = submissions[index];
-          return Material(
-            //color: Colors.blue,
-            child: Container(
-              margin: EdgeInsets.all(10),
-              decoration:BoxDecoration(
-                color: Colors.black.withOpacity(0.4),
-                borderRadius: BorderRadius.all(Radius.circular(10.0)),
-              ),
-              child: ListTile(
-                enabled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
+                              bool result =await InternetConnectionChecker().hasConnection;
+                              if ( result==true) {
+                                List data = jsonDecode(submission.jsonData);
+                                var dataUpload = <Map<String, dynamic>>[];
 
-                title: Text('${FormViewController.formData?['title'] ?? 'No Title'}'),
+                                for (var item in data) {
+                                  if (item["file"].isNotEmpty) {
+                                    print("not null");
+                                    print('File: ${item["file"]}');
+                                    final file = File(item["file"]);
+                                    HttpResponse response = await HttpClient.uploadtoS3(
+                                        file);
+                                    item["answer"] = response.data;
+                                    // print(item["answer"]);
+                                  }
 
-                subtitle: Text('Submission ID: ${submission.id}'),
-                trailing: IconButton(
+                                  final fieldData = <String, dynamic>{
+                                    'id': item["id"],
+                                    'question': item["question"],
+                                    'answer': item["answer"],
+                                  };
 
+                                  dataUpload.add(fieldData);
+                                }
 
-
-                  //icon: Icon(IconData(0xe175, fontFamily: 'MaterialIcons'), color: Colors.white, size: 30,),
-                  icon: Icon(Icons.upload, color: Colors.white, size: 30,),
-
-                  onPressed: () async {
-
-                    bool result =await InternetConnectionChecker().hasConnection;
-                    if ( result==true) {
-                      List data = jsonDecode(submission.jsonData);
-                      var dataUpload = <Map<String, dynamic>>[];
-
-                      for (var item in data) {
-                        if (item["file"].isNotEmpty) {
-                          print("not null");
-                          print('File: ${item["file"]}');
-                          final file = File(item["file"]);
-                          HttpResponse response = await HttpClient.uploadtoS3(
-                              file);
-                          item["answer"] = response.data;
-                          // print(item["answer"]);
-                        }
-
-                        final fieldData = <String, dynamic>{
-                          'id': item["id"],
-                          'question': item["question"],
-                          'answer': item["answer"],
-                        };
-
-                        dataUpload.add(fieldData);
-                      }
-
-                      final jsonData = jsonEncode(dataUpload);
+                                final jsonData = jsonEncode(dataUpload);
 
 
 
-                      FormController.submitForm(submission.formId, jsonData);
-                      box?.remove(submission.id);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Submission Uploaded.')),);
-                    }
-                    else{
+                                FormController.submitForm(submission.formId, jsonData);
+                                box?.remove(submission.id);
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text('Submission Uploaded.')),);
+                              }
+                              else{
 
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('No internet connection available.')),
-                    );}
-                  },
-                ),
-                // You can display more details here as needed
-              ),
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text('No internet connection available.')),
+                                );}
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              bool result =await InternetConnectionChecker().hasConnection;
+              if ( result==true) {
+                // Logic for your ElevatedButton at the end
+                for (var submission in submissions) {
+                  List data = jsonDecode(submission.jsonData);
+                  var dataUpload = <Map<String, dynamic>>[];
+                  for (var item in data) {
+                    if (item["file"].isNotEmpty) {
+                    print("not null");
+                    print('File: ${item["file"]}');
+                    final file = File(item["file"]);
+                    HttpResponse response = await HttpClient.uploadtoS3(
+                      file);
+                    item["answer"] = response.data;
+                    // print(item["answer"]);
+                    }
+
+                  final fieldData = <String, dynamic>{
+                  'id': item["id"],
+                  'question': item["question"],
+                  'answer': item["answer"],
+                  };
+
+                  dataUpload.add(fieldData);
+                  }
+
+                  final jsonData = jsonEncode(dataUpload);
+
+
+
+                  FormController.submitForm(submission.formId, jsonData);
+                  box?.remove(submission.id);
+                  print('Form ID: ${submission.id}');
+                  ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Submission Uploaded.')),);
+                  }
+              }
+              else{
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('No internet connection available.')),
+                );}
+            },
+            child: Text('Submit All'),
+          ),
+        ],
       ),
     );
+
   }
 }
 
